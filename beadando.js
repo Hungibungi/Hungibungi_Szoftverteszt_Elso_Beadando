53032a4af280535369ba02ebfa85ca8ec4de1deb
Kertész Gábor <h983581@inf.u-szeged.hu>
2021-10-20 16:50:36 +0200

    #7 green + blue

--- beadando.js
+++ beadando.js
@@ -3,24 +3,40 @@ function hello(names) {
         return "Hello, my friend.";
     }
     if(names.length == 1){
-        if(names[0].toUpperCase() == names[0]){
+        if(shout(names) == true){
             return "HELLO " + names + "!";
         }
         return "Hello, " + names[0] + ".";
     }
     if(names.length == 2){
-        if(names[0].toUpperCase() == names[0] && names[1].toUpperCase() == names[1]){
+        if(shout(names) == true){
             return "HELLO " + names[0] + " AND " + names[1] + "!";
         }
         return "Hello, " + names[0] + " and " + names[1] + ".";
     }
     if(names.length > 2){
+        if(shout(names) == true){
+            var returnee = "HELLO ";
+            for(i=0;i<names.length-2;i++){
+                returnee = returnee.concat(names[i] + ", ");
+            }
+            return returnee.concat(names[names.length-2] + " AND " + names[names.length-1] + "!");
+        }
         var returnee = "Hello, ";
         for(i=0;i<names.length-2;i++){
             returnee = returnee.concat(names[i] + ", ");
         }
         return returnee.concat(names[names.length-2] + " and " + names[names.length-1] + ".");
     }
+}
+
+function shout(names) {
+    for(i=0;i<names.length;i++){
+        if(names[i].toUpperCase() != names[i]){
+            return false;
+        }
+    }
+    return true;
 }
 
 module.exports = hello;
