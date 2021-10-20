05dfab80765322e0fcb92bce8e7db423657eed6f
Kertész Gábor <h983581@inf.u-szeged.hu>
2021-10-20 17:16:24 +0200

    #8 green, refactor not needed

--- beadando.js
+++ beadando.js
@@ -9,6 +9,9 @@ function hello(names) {
         return "Hello, " + names[0] + ".";
     }
     if(names.length == 2){
+        if(mixed(names)[0] == true){
+            return hello(mixed(names)[1]) + " AND " + hello(mixed(names)[2]);
+        }
         if(shout(names) == true){
             return "HELLO " + names[0] + " AND " + names[1] + "!";
         }
@@ -37,6 +40,24 @@ function shout(names) {
         }
     }
     return true;
+}
+
+function mixed(names) {
+    var lowercase = [];
+    var uppercase = [];
+    for(i=0;i<names.length;i++){
+        if(names[i].toUpperCase() == names[i]){
+            uppercase.push(names[i]);
+        }
+        if(names[i].toUpperCase() != names[i]) {
+            lowercase.push(names[i]);
+        }
+    }
+    if(lowercase.length == 0 || uppercase.length == 0){
+        return [false];
+    }
+    return [true, lowercase, uppercase];
+
 }
 
 module.exports = hello;
