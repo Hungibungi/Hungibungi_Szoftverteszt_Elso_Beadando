970d6acdd245228d00c557e0f7c7520299c19614
Kertész Gábor <h983581@inf.u-szeged.hu>
2021-10-20 16:03:32 +0200

    #2 green, refactor not needed

--- beadando.js
+++ beadando.js
@@ -2,6 +2,9 @@ function hello(names) {
     if(names.length == 0){
         return "Hello, my friend.";
     }
+    if(names.length == 1){
+        return "Hello, " + names[0] + ".";
+    }
 }
 
 module.exports = hello;
