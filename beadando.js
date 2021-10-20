9f4f4ff50eb1855ba10b476543f563b03871cb9f
Kertész Gábor <h983581@inf.u-szeged.hu>
2021-10-20 16:22:52 +0200

    #4 green

--- beadando.js
+++ beadando.js
@@ -8,6 +8,15 @@ function hello(names) {
     if(names.length == 2){
         return "Hello, " + names[0] + " and " + names[1] + ".";
     }
+    if(names.length > 2){
+        var returnee = "Hello, ";
+        for(i=0;i<names.length-2;i++){
+            returnee = returnee.concat(names[i] + ", ");
+        }
+        returnee = returnee.concat(names[names.length-2] + " and ");
+        returnee = returnee.concat(names[names.length-1] + ".");
+        return returnee;
+    }
 }
 
 module.exports = hello;
