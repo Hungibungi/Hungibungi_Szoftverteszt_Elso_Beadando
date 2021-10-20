59c377c1062b195b024da10ba2cdf2c9eaf7f68d
Kertész Gábor <h983581@inf.u-szeged.hu>
2021-10-20 15:52:49 +0200

    #2 red

--- beadando.test.js
+++ beadando.test.js
@@ -3,3 +3,7 @@ const hello = require('./beadando');
 test('no imput', () => {
     expect(hello("")).toBe('Hello, my friend.');
 });
+
+test('one name', () => {
+    expect(hello("Stella")).toBe('Hello, Stella.');
+});
